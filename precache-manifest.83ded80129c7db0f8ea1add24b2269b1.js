3372f8fab05222fc507a5c421fa1ebd52f89bea9
roi <bendaroi@gmail.com>
2020-11-11 16:04:30 +0200

    Updates

---
+++ precache-manifest.83ded80129c7db0f8ea1add24b2269b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165eac85ac17009c635e6bf60c18ac68",
+    "url": "/Roi/index.html"
+  },
+  {
+    "revision": "cee7e333bb43f633b001",
+    "url": "/Roi/static/css/main.1e760b07.chunk.css"
+  },
+  {
+    "revision": "df9cafb5d56104d17c26",
+    "url": "/Roi/static/js/2.98141934.chunk.js"
+  },
+  {
+    "revision": "3086e5034cdbd828f5d4b50fdc405362",
+    "url": "/Roi/static/js/2.98141934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee7e333bb43f633b001",
+    "url": "/Roi/static/js/main.6717dbb8.chunk.js"
+  },
+  {
+    "revision": "9c439d57c80da6074c21",
+    "url": "/Roi/static/js/runtime-main.33a0cbef.js"
+  },
+  {
+    "revision": "c8d77d3eccf2cf9962956138746e9cd8",
+    "url": "/Roi/static/media/PaytoneOne-Regular.c8d77d3e.ttf"
+  },
+  {
+    "revision": "e74285672c560f6dc6480129b15d963b",
+    "url": "/Roi/static/media/smoke.e7428567.jpg"
+  }
+]);
